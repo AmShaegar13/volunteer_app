355dccc33fd0b51d1a1266abfafe3ae5f4ab3252
AmShaegar <amshaegar@wrenchbox.de>
2014-09-14 17:44:06 +0200

    extending smurf tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -12,10 +12,23 @@ class UserTest < ActiveSupport::TestCase
     assert_not_nil User.find_by(name: 'Random User')
   end
 
-  test 'should test smurf-main association' do
-    user = User.find_by(name: 'AmShaegar')
+  test 'should have smurf-main association' do
+    user = users(:amshaegar)
     assert user.smurfs.include?(users(:smurf_1))
     assert_equal user, user.smurfs.sample.main
+  end
+
+  test 'should set smurf-main association' do
+    user = users(:humpel)
+    smurf = users(:smurf_new)
+
+    assert_equal 0, user.smurfs.size
+    assert_nil smurf.main
+
+    user.smurfs << smurf
+
+    assert user.smurfs.size == 1
+    assert_equal user, smurf.main
   end
 
   test 'should fail to create user without name' do
