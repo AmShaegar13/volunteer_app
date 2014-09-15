55ae327564b721f9fa642b524bf46be80b43dce1
AmShaegar <amshaegar@wrenchbox.de>
2014-09-15 19:20:19 +0200

    improved access to summoner api

--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -6,14 +6,23 @@ class Summoner < ActiveResource::Base
   self.site = @@resources['riot_api']['base_url']
   self.collection_name ='summoner'
   self.include_format_in_path = false
+  self.logger = Logger.new(STDERR) if Rails.env == 'development'
 
   @@api_key = @@resources['riot_api']['api_key']
 
-  ##
-  # GET request to riot API.
-  #
-  def self.get(method_name, options = {})
-    options[:api_key] = @@api_key
-    HashWithIndifferentAccess.new(super(method_name, options))
+  class << self
+    ##
+    # GET request to riot API.
+    #
+    def get(method_name, options = {})
+      options[:api_key] = @@api_key
+      HashWithIndifferentAccess.new(super(method_name, options))
+    end
+
+    def find_by_name(*names)
+      names = names * ',' if names.is_a? Array
+      get("by-name/#{names}")
+    end
+    alias_method :find_by_names, :find_by_name
   end
 end
