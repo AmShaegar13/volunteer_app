7c250d6f60403e8c1f81b3d64c30778e555e614f
AmShaegar <amshaegar@wrenchbox.de>
2014-09-15 19:08:06 +0200

    do not commit secrets

--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -1,17 +1,19 @@
 require 'active_resource'
 
 class Summoner < ActiveResource::Base
-  self.site = 'https://euw.api.pvp.net/api/lol/euw/v1.4/'
+  @@resources = YAML.load_file('config/resources.yml')
+
+  self.site = @@resources['riot_api']['base_url']
   self.collection_name ='summoner'
   self.include_format_in_path = false
 
-  self.logger = Logger.new(STDERR)
+  @@api_key = @@resources['riot_api']['api_key']
 
   ##
   # GET request to riot API.
   #
   def self.get(method_name, options = {})
-    options[:api_key] = '' # TODO find a way to load api_key from external source
-    super(method_name, options)
+    options[:api_key] = @@api_key
+    HashWithIndifferentAccess.new(super(method_name, options))
   end
 end
