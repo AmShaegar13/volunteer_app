a00fd1ddd3b8f79c94ab4305dc90f9881b7e780d
AmShaegar <amshaegar@wrenchbox.de>
2014-09-17 23:14:28 +0200

    basic user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def index
     redirect_to :root
   end
+
+  def show
+    @user = User.find(params[:id])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,11 @@
 class User < ActiveRecord::Base
   has_many :smurfs, class_name: 'User', foreign_key: 'main_id'
   belongs_to :main, class_name: 'User'
-  has_many :bans, -> { order 'created_at DESC' }
+  has_many :bans, -> { order 'created_at + INTERVAL duration DAY' }
 
   validates :name, presence: true
+
+  def banned?
+    bans.last.end > Time.now
+  end
 end
