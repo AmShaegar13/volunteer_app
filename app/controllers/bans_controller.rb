b1af39a660ea821a506fb91a70a1d852d6d86695
AmShaegar <amshaegar@wrenchbox.de>
2014-09-17 19:19:59 +0200

    add new bans

--- app/controllers/bans_controller.rb
+++ app/controllers/bans_controller.rb
@@ -3,4 +3,14 @@ class BansController < ApplicationController
     limit = params[:limit] ? params[:limit] : 15
     @bans = Ban.ordered_by_end.limit(limit)
   end
+
+  def new
+    @ban = Ban.new
+  end
+
+  def create
+    user = User.new params.require(:ban).require(:user).permit(:name)
+    Ban.create! params.require(:ban).permit(:duration, :user, :reason, :link).merge(user: user)
+    redirect_to :root
+  end
 end
