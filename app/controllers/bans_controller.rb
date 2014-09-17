d6831106eafa97aca28520ea3f3a0818b86028af
AmShaegar <amshaegar@wrenchbox.de>
2014-09-18 01:45:00 +0200

    simplified rendering; include ban creation form into index

--- app/controllers/bans_controller.rb
+++ app/controllers/bans_controller.rb
@@ -2,6 +2,7 @@ class BansController < ApplicationController
   def index
     limit = params[:limit] ? params[:limit] : 15
     @bans = Ban.ordered_by_end.limit(limit)
+    self.new
   end
 
   def new
