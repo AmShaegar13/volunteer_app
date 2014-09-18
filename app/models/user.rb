e5544898d94c78e12b3db666f27f608800c04acb
AmShaegar <amshaegar@wrenchbox.de>
2014-09-18 02:45:06 +0200

    fixed ordering to always set perma as last ban

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_many :smurfs, class_name: 'User', foreign_key: 'main_id', dependent: :nullify
   belongs_to :main, class_name: 'User'
   # always order bans descending by end of ban; duration = -1 ensures perma bans on top
-  has_many :bans, -> { order 'duration = -1 DESC, created_at + INTERVAL duration DAY' }, dependent: :destroy
+  has_many :bans, -> { order 'duration = -1, created_at + INTERVAL duration DAY' }, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
 
