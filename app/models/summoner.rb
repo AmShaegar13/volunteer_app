493ed2b0531aabce0b1067579ac6825ffab0ad07
Glöe, Finn <finn.gloee@1und1.de>
2014-09-19 16:22:15 +0200

    improved Summoner

--- app/models/summoner.rb
+++ app/models/summoner.rb
@@ -2,13 +2,12 @@ require 'active_resource'
 
 class Summoner < ActiveResource::Base
   @@resources = YAML.load_file('config/resources.yml')
+  @@api_key = @@resources['riot_api']['api_key']
 
   self.site = @@resources['riot_api']['base_url']
   self.collection_name ='summoner'
   self.include_format_in_path = false
   self.logger = Logger.new(STDERR) if Rails.env == 'development'
-
-  @@api_key = @@resources['riot_api']['api_key']
 
   class << self
     ##
@@ -19,10 +18,19 @@ class Summoner < ActiveResource::Base
       HashWithIndifferentAccess.new(super(method_name, options))
     end
 
-    def find_by_name(*names)
-      names = names * ',' if names.is_a? Array
-      get("by-name/#{names}")
+    def find_by_name(name)
+      names = [name]
+      names << fix_encoding(name, Encoding::ISO8859_1) rescue nil
+      names << fix_encoding(name, Encoding::ISO8859_2) rescue nil
+      names << fix_encoding(name, Encoding::WINDOWS_1252) rescue nil
+      names = URI::encode(names * ',')
+      new(get("by-name/#{names}"))
     end
     alias_method :find_by_names, :find_by_name
+
+    def fix_encoding(str, enc)
+      str.encode(enc).force_encoding(Encoding::UTF_8)
+    end
+    private :fix_encoding
   end
 end
