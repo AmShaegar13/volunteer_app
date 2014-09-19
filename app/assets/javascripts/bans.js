2392638654e0bb1f320c58a003551baaf3be06c0
Glöe, Finn <finn.gloee@1und1.de>
2014-09-19 17:37:01 +0200

    preselect recommended ban

--- app/assets/javascripts/bans.js
+++ app/assets/javascripts/bans.js
@@ -1,2 +1,13 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(function() {
+    $('#ban_user_id').on('change', function()  {
+        latest_ban = $('#ban_user_id option:selected').data('latest-ban');
+        console.log(latest_ban);
+        next = $('#ban_duration option[value='+latest_ban+']').next().val();
+        console.log(next);
+        if(next) {
+            $('#ban_duration').val(next);
+        } else {
+            $('#ban_duration').val(-1); // perma
+        }
+    });
+});
