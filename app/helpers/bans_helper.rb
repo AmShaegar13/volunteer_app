e038557c906e50273d9801bba3a908e1979ef610
AmShaegar <amshaegar@wrenchbox.de>
2014-09-20 01:49:13 +0200

    suggest next ban level

--- app/helpers/bans_helper.rb
+++ app/helpers/bans_helper.rb
@@ -1,2 +1,8 @@
 module BansHelper
+  BAN_DURATIONS = [1, 3, 7, 14, -1]
+
+  def increase_duration(duration)
+    result = BAN_DURATIONS.at BAN_DURATIONS.index(duration)+1
+    result || duration
+  end
 end
