8c6bcb035eb88a8b1d6031960f28b661c1794be4
AmShaegar <amshaegar@wrenchbox.de>
2014-09-20 22:01:12 +0200

    reorder requires to fix issues in production

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,12 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+require 'resources'
+Resources.data = YAML.load_file('config/resources.yml')
+
 # Initialize the Rails application.
 Rails.application.initialize!
 
 require 'pp'
-
-require 'resources'
-Resources.data = YAML.load_file('config/resources.yml')
 
 require 'authentication'
