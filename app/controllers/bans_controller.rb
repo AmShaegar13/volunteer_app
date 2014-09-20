90e827ca10ff9afe15caad8e07c84d1693099081
AmShaegar <amshaegar@wrenchbox.de>
2014-09-20 04:47:45 +0200

    fixed params error in bans#create

--- app/controllers/bans_controller.rb
+++ app/controllers/bans_controller.rb
@@ -11,13 +11,10 @@ class BansController < ApplicationController
 
   def create
     user = User.find_or_create params.require(:ban).require(:user).require(:name)
-
-    begin
-      Ban.create! params.require(:ban).permit(:duration, :user, :reason, :link).merge(user: user)
-    rescue => ex
-      flash[:error] = ex.message
-    end
-
+    Ban.create! params.require(:ban).permit(:duration, :user, :reason, :link).merge(user: user)
+  rescue => ex
+    flash[:error] = ex.message
+  ensure
     redirect_to :root
   end
 end
