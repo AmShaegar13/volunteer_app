1c1c6fdd9428fd6ea5b0d1f6762da1acbf58d7f8
AmShaegar <amshaegar@wrenchbox.de>
2014-09-22 20:20:24 +0200

    added ban colors

--- app/helpers/bans_helper.rb
+++ app/helpers/bans_helper.rb
@@ -1,8 +1,13 @@
 module BansHelper
   BAN_DURATIONS = [1, 3, 7, 14, -1]
+  BAN_COLORS = %w[#b6d7a8 #ffe599 #f9cb9c #ea9999 #cc4125]
 
   def increase_duration(duration)
     result = BAN_DURATIONS.at BAN_DURATIONS.index(duration)+1
     result || duration
+  end
+
+  def color_by_duration(duration)
+    BAN_COLORS[BAN_DURATIONS.index(duration)]
   end
 end
