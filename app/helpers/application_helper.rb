7689efc9a7c4b3afcac43883babb2d66cda8b916
AmShaegar <amshaegar@wrenchbox.de>
2014-09-23 23:09:21 +0200

    fixed perma ban duration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def format_duration(d)
     case d
-      when -1 then 'permanent'
+      when 0 then 'permanent'
       when 1 then "#{d} day"
       else "#{d} days"
     end
