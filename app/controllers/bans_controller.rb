726857429ade136bb1603d71c819b9cc97991957
AmShaegar <amshaegar@wrenchbox.de>
2014-09-27 18:10:11 +0200

    add user/smurf association on ban creation

--- app/controllers/bans_controller.rb
+++ app/controllers/bans_controller.rb
@@ -11,7 +11,18 @@ class BansController < ApplicationController
   end
 
   def create
-    user = User.find_or_create(params.require(:ban).require(:user).permit(:id, :name))
+    user_params = params.require(:ban).require(:user).permit(:id, :name, :main)
+    user = User.find_or_create(user_params.slice(:id, :name))
+    raise ArgumentError, "User '#{user_params[:name]}' does not exist." if user.nil?
+    if user_params.key?(:main)
+      main = User.find_or_create(name: user_params[:main])
+      unless main.nil?
+        user.main = main
+        user.save!
+      else
+        flash[:notice] = "User '#{user_params[:main]}' does not exist."
+      end
+    end
     Ban.create!(params.require(:ban).permit(:duration, :reason, :link).merge(user: user)) do |ban|
       Action.create!(tool_user: current_user, action: 'create', reference: ban)
     end
