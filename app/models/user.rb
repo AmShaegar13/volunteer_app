a2999362c835b55fc8aceb0875cfeae85ed1a007
AmShaegar <amshaegar@wrenchbox.de>
2014-11-23 15:59:29 +0100

    validate presence region

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   validates :id, presence: true, uniqueness: true
   validates :name, presence: true, uniqueness: true
   validates :level, presence: true, inclusion: { in: 1..30 }
+  validates :region, presence: true, inclusion: { in: %w(na euw eune) }
 
   def banned?
     !bans.empty? && bans.last.active?
