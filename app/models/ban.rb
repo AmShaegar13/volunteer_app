7a27e494e39b0541df855519aff81b36a1f43381
AmShaegar <amshaegar@wrenchbox.de>
2014-11-29 12:17:04 +0100

    cache common ban reasons

--- app/models/ban.rb
+++ app/models/ban.rb
@@ -3,7 +3,7 @@ class Ban < ActiveRecord::Base
   belongs_to :creator, class_name: ToolUser.name, foreign_key: 'creator_id'
   has_many :actions, as: :reference, dependent: :destroy
 
-  scope :common_reasons, -> { select(:reason).group(:reason).order('COUNT(reason) DESC').limit(15).map(&:reason) }
+  scope :common_reasons, -> { select(:reason).group(:reason).order('COUNT(reason) DESC').limit(15) }
   scope :ordered_by_end, -> { order('created_at + INTERVAL duration DAY DESC') }
 
   with_options presence: true do |ban|
