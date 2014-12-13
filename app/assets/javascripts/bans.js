eee6b6f2b9302034084c733cd31c21c83cde55d9
Glöe, Finn <finn.gloee@1und1.de>
2014-12-13 22:03:38 +0100

    added consideration of ban proposal

--- app/assets/javascripts/bans.js
+++ app/assets/javascripts/bans.js
@@ -2,14 +2,18 @@ var getUsers = function(request, response) {
     if(!request.term) {
         return;
     }
-    $.getJSON('search?search_query=' + request.term, function(result) {
+    var form_fields = $('#new_ban').find('input,select').not('[data-protected=true]');
+    form_fields.prop('disabled', true);
+    $.getJSON('search?include_proposal=true&search_query=' + request.term, function(result) {
         var names = [];
-        $.each(result, function (i, name) {
+        $.each(result, function (i, user) {
             names.push({
-                label: name,
-                value: name
+                label: user.name,
+                value: user.name,
+                proposal: user.proposal
             });
         });
+        form_fields.removeProp('disabled');
         response(names);
     });
 };
