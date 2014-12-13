6955499080841cd8f71cc2eb34d7621b964fca68
Glöe, Finn <finn.gloee@1und1.de>
2014-12-13 22:15:00 +0100

    removed disabling input fields while loading

--- app/assets/javascripts/bans.js
+++ app/assets/javascripts/bans.js
@@ -2,8 +2,6 @@ var getUsers = function(request, response) {
     if(!request.term) {
         return;
     }
-    var form_fields = $('#new_ban').find('input,select').not('[data-protected=true]');
-    form_fields.prop('disabled', true);
     $.getJSON('search?include_proposal=true&search_query=' + request.term, function(result) {
         var names = [];
         $.each(result, function (i, user) {
@@ -13,7 +11,6 @@ var getUsers = function(request, response) {
                 proposal: user.proposal
             });
         });
-        form_fields.removeProp('disabled');
         response(names);
     });
 };
